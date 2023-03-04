7a16a666cd17443f8934b4b2bceabdc049d67cbe
andreivince <andreivince21@gmail.com>
2023-03-04 17:33:04 -0500

    Printing

--- index.py
+++ index.py
@@ -14,6 +14,27 @@ symbol_count = { # Frequency of symbols
     "D": 8
 }
 
+symbol_value = { # Value of syymbols
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0
+    for line in range(lines):
+        symbol = columns[0][lines]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else:
+            winnings += values[symbol] * bet
+            
+    return winnings
+
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
     for symbol, symbol_count in symbols.items():
@@ -37,9 +58,10 @@ def print_slot_machine(columns): # Transposing a dataset means swapping its rows
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
-                print(column[row], " | ")
+                print(column[row], end = " | ")
             else:
-                print(column[row])
+                print(column[row], end = "")
+        print()
 
                 
         
